973c155bdc6b03d41fcdd0f1b8f0c4bc9aab76ba
Sriram-Y <sriramy2001@gmail.com>
2023-01-14 21:19:23 -0800

    changes to test git

--- app/src/main/java/com/ysri/spotifyautoeq/MainActivity.kt
+++ app/src/main/java/com/ysri/spotifyautoeq/MainActivity.kt
@@ -12,7 +12,7 @@ import com.spotify.android.appremote.api.SpotifyAppRemote
 class MainActivity : AppCompatActivity() {
     private val CLIENT_ID = "13684737619449b59f2aa606ed3f9b2c"
     private val REDIRECT_URI = "http://localhost:5555"
-    private var mSpotifyAppRemote: SpotifyAppRemote? = null;
+    private var mSpotifyAppRemote: SpotifyAppRemote? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,11 +47,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun connected() {
-        mSpotifyAppRemote?.getPlayerApi()?.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+        mSpotifyAppRemote?.getPlayerApi()?.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
     }
 
     override fun onStop() {
         super.onStop()
-        // Aaand we will finish off here.
+        // And we will finish off here.
     }
 }
